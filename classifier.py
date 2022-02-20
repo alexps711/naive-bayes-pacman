b3cc3ee180cfc5ec3339cfcfedf432c978663e5b
alexps711 <alejanper@hotmail.com>
2022-02-20 12:40:04 +0000

    init

---
+++ classifier.py
@@ -0,0 +1,19 @@
+# classifier.py
+# Lin Li/26-dec-2021
+#
+# Use the skeleton below for the classifier and insert your code here.
+import api
+
+class Classifier:
+    def __init__(self):
+        pass
+
+    def reset(self):
+        pass
+    
+    def fit(self, data, target):
+        pass
+
+    def predict(self, data, legal=None):
+        return 1
+        
